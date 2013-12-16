7ec0c1eaca5c4d7eedd7efcd9917fd64195f66e1
Daniel Woelfel <dwwoelfel@gmail.com>
2013-12-15 21:59:24 -0800

    adds CI through CircleCI

---
+++ script/run-etcd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+wget https://github.com/coreos/etcd/releases/download/v0.2.0-rc2/etcd-v0.2.0-rc2-Linux-x86_64.tar.gz
+
+tar xvfz etcd-*.tar.gz
+cd etcd-*
+nohup bash -c "./etcd &"
