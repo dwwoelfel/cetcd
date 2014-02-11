05ad4b691ca2c5e1fec2587e36915da5700c0adf
Daniel Woelfel <dwwoelfel@gmail.com>
2014-02-11 12:04:25 -0800

    bump minor version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cetcd "0.2.0"
+(defproject cetcd "0.2.1"
   :description "Clojure wrapper for etcd"
   :url "http://github.com/dwwoelfel/cetcd"
   :license {:name "Eclipse Public License"
