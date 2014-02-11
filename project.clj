1e6b9c6e0d0ba6bdbbc2ac4dc330fde74c00150d
Daniel Woelfel <dwwoelfel@gmail.com>
2014-02-11 12:03:24 -0800

    update http-kit

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit "2.1.13"]
+                 [http-kit "2.1.16"]
                  [clj-http "0.7.8"]
                  [cheshire "5.2.0"]
                  [slingshot "0.10.3"]
